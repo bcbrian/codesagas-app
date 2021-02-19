0a676ad4377485de69c1c9095fbf6d894f0876ce
Brian Bartholomew <me@bcbrian.com>
2021-02-19 08:02:50 -0700

    test 1 dones

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,96 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  fireEvent,
+  render,
+  screen
+} from "@testing-library/react";
+import App from "./App";
 
-const QUIZ = {
+const quiz = {
   title: "Quiz Title",
-  subtitle: "The longer subtitle for the quiz.",
+  subtitle:
+    "The longer subtitle for the quiz.",
   startButtonText: "start quiz",
+  questions: [
+    {
+      question:
+        "a is the correct answer",
+      type: "multiple choice", // store this and reference this...
+      options: [
+        {
+          text: "a",
+          isCorrect: true
+        },
+        {
+          text: "b",
+          isCorrect: true
+        },
+        {
+          text: "c",
+          isCorrect: true
+        },
+        {
+          text: "d",
+          isCorrect: true
+        }
+      ]
+    }
+  ]
+};
+
+function getByTextLowercase(str) {
+  const reg = RegExp(str, "i");
+  return screen.getByText(reg);
 }
 
-function regexI(str){
-  return RegExp(str, "i");
-}
-
-test('user can start a mini test', () => {
+test("user can start a mini test", () => {
   //arrange
-  render(<App quiz={QUIZ} />);
-  
-  //act
-  
-  //assert
-  const titleElement = screen.getByText(regexI(QUIZ.title));
-  const subtitleElement = screen.getByText(regexI(QUIZ.subtitle));
-  const startButtonElement = screen.getByText(regexI(QUIZ.startButtonText));
-  expect(titleElement).toBeInTheDocument();
-  expect(subtitleElement).toBeInTheDocument();
-  expect(startButtonElement).toBeInTheDocument();
-  
-  //act
-  fireEvent.click(startButtonElement)
-  
-  //assert
-  const q1Text =   const titleElement = screen.getByText(regexI(QUIZ[0].title));
+  render(<App quiz={quiz} />);
 
+  //act
+
+  //assert
+  const titleElement = getByTextLowercase(
+    quiz.title
+  );
+  const subtitleElement = getByTextLowercase(
+    quiz.subtitle
+  );
+  const startButtonElement = getByTextLowercase(
+    quiz.startButtonText
+  );
+
+  expect(
+    titleElement
+  ).toBeInTheDocument();
+  expect(
+    subtitleElement
+  ).toBeInTheDocument();
+  expect(
+    startButtonElement
+  ).toBeInTheDocument();
+
+  //act
+  fireEvent.click(startButtonElement);
+
+  //assert
+  const q1Text = getByTextLowercase(
+    quiz.questions[0].question
+  );
+  const q1Option1 = getByTextLowercase(
+    quiz.questions[0].options[0].text
+  );
+  const q1Option2 = getByTextLowercase(
+    quiz.questions[0].options[1].text
+  );
+  const q1Option3 = getByTextLowercase(
+    quiz.questions[0].options[2].text
+  );
+  const q1Option4 = getByTextLowercase(
+    quiz.questions[0].options[3].text
+  );
+  expect(q1Text).toBeInTheDocument();
+  expect(q1Option1).toBeInTheDocument();
+  expect(q1Option2).toBeInTheDocument();
+  expect(q1Option3).toBeInTheDocument();
+  expect(q1Option4).toBeInTheDocument();
 });
